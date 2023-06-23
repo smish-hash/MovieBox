102aabddd0a60db7a5a84fd98a3306b10e5933cf
smish <s.mish22222@gmail.com>
2023-06-23 11:55:20 +0530

    Error Handling and Loading Effect on Images

---
+++ app/src/main/java/com/example/moviebox/ui/screen/Global.kt
@@ -0,0 +1,66 @@
+package com.example.moviebox.ui.screen
+
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.moviebox.R
+
+@Composable
+fun ShimmerBrush(showShimmer: Boolean = true,targetValue:Float = 1000f): Brush {
+    return if (showShimmer) {
+        val shimmerColors = listOf(
+            Color.LightGray.copy(alpha = 0.6f),
+            Color.LightGray.copy(alpha = 0.2f),
+            Color.LightGray.copy(alpha = 0.6f),
+        )
+
+        val transition = rememberInfiniteTransition()
+        val translateAnimation = transition.animateFloat(
+            initialValue = 0f,
+            targetValue = targetValue,
+            animationSpec = infiniteRepeatable(
+                animation = tween(800), repeatMode = RepeatMode.Reverse
+            )
+        )
+        Brush.linearGradient(
+            colors = shimmerColors,
+            start = Offset.Zero,
+            end = Offset(x = translateAnimation.value, y = translateAnimation.value)
+        )
+    } else {
+        Brush.linearGradient(
+            colors = listOf(Color.Transparent,Color.Transparent),
+            start = Offset.Zero,
+            end = Offset.Zero
+        )
+    }
+}
+
+@Composable
+fun ImageLoadingError(errorImage: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = errorImage),
+            contentDescription = stringResource(id = R.string.image_error)
+        )
+    }
+}
--- app/src/main/java/com/example/moviebox/ui/screen/MovieList/MovieCard.kt
+++ app/src/main/java/com/example/moviebox/ui/screen/MovieList/MovieCard.kt
@@ -1,36 +1,53 @@
 package com.example.moviebox.ui.screen.MovieList
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
 import com.example.moviebox.R
 import com.example.moviebox.data.model.movielist.Result
+import com.example.moviebox.ui.screen.ImageLoadingError
+import com.example.moviebox.ui.screen.ShimmerBrush
 import com.example.moviebox.util.Constants
 import com.example.moviebox.util.toShortenedString
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.coil.CoilImage
+import com.skydoves.landscapist.coil.CoilImageState
+import com.skydoves.landscapist.components.rememberImageComponent
+import com.skydoves.landscapist.placeholder.shimmer.ShimmerPlugin
 
 @Composable
 fun MovieCard(
@@ -45,7 +62,11 @@ fun MovieCard(
             .width(250.dp)
             .clickable { onMovieClick(movie.id) }
     ) {
+
         val imageUrl = Constants.BASE_IMAGE_URL + movie.posterPath
+        val shouldShimmer = remember {
+            mutableStateOf(true)
+        }
 
         CoilImage(
             imageModel = { imageUrl },
@@ -54,7 +75,24 @@ fun MovieCard(
                 alignment = Alignment.Center
             ),
             previewPlaceholder = R.drawable.nature,
-            modifier = Modifier.clip(RoundedCornerShape(10.dp))
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .heightIn(min = 165.dp),
+            loading = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(ShimmerBrush(showShimmer = shouldShimmer.value)),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    ShimmerBrush()
+                }
+            },
+            failure = {
+                ImageLoadingError(errorImage = R.drawable.baseline_error_outline_24)
+            }
+
         )
 
         Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_small)))
