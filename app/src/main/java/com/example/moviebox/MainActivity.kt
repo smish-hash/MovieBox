cbdb67c3d44d78140f0f98c4f22a8977fd800aa7
Satyajit Mishra <s.mish22222@gmail.com>
2023-07-07 16:32:30 +0530

    Misc

--- app/src/main/java/com/example/moviebox/MainActivity.kt
+++ app/src/main/java/com/example/moviebox/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.example.moviebox
 
 import android.os.Bundle
-import android.view.View
-import androidx.core.view.OnApplyWindowInsetsListener
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -14,7 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MainActivity : BaseActivityBinding<ActivityMainBinding>(), OnApplyWindowInsetsListener {
+class MainActivity : BaseActivityBinding<ActivityMainBinding>() {
 
     private lateinit var navController: NavController
     override fun getViewBinding(): ActivityMainBinding {
@@ -26,7 +22,6 @@ class MainActivity : BaseActivityBinding<ActivityMainBinding>(), OnApplyWindowIn
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
-        ViewCompat.setOnApplyWindowInsetsListener(window.decorView, this)
     }
 
     fun updateTitle(title: String) {
@@ -36,21 +31,4 @@ class MainActivity : BaseActivityBinding<ActivityMainBinding>(), OnApplyWindowIn
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
-    override fun onApplyWindowInsets(v: View, insets: WindowInsetsCompat): WindowInsetsCompat {
-        /*var gestureLeft = 0
-        if (Build.VERSION.SDK_INT >= 29) {
-            gestureLeft = this.window.decorView.rootWindowInsets.systemGestureInsets.left
-        }
-
-        if (gestureLeft == 0) {
-            // Doesn't use gesture type navigation
-            showSmallToast("Has nav buttons")
-        } else {
-            // Uses gesture type navigation
-            showSmallToast("use gesture")
-        }*/
-        return insets
-    }
-
 }
